aebc177df0787b5efbdfdd4dccb624ca74e838b5
SiyangH <siyangshirleyhao@gmail.com>
2024-05-27 20:20:22 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,12 +4,12 @@ st.set_page_config(
     page_title="Exploring Rideshare in New York",
 )
 
-st.write("Exploring Rideshare in New York")
+st.title("Exploring Rideshare in New York")
 
 st.markdown(
     """
     This Streamlit Web App is the UI of Group 9's MSBD5003 Course Project
-    ## Please select the variable you would like to explore:
+    Please select the variable you would like to explore (options are also availale on the sidebar):
     - [Number of Orders](/Number_of_Orders)
 """
 )
